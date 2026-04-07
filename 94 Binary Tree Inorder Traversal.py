a2f510104dde0fea001d5012cde41720ef6968bd
Pilei Zhang <pzhang32@hawk.iit.edu>
2026-04-07 16:48:58 -0500

    94 Binary Tree Inorder Traversal, 2 methods

---
+++ 94 Binary Tree Inorder Traversal.py
@@ -0,0 +1,47 @@
+# method 1: recursion
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        lst = []
+
+        def inorder(root: Optional[TreeNode]):
+            if root == None:
+                return
+            
+            inorder(root.left)
+            lst.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+
+        return lst
+
+
+# method 2 iteration
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        stack = []
+        
+        if root == None:
+            return []
+
+        while root or stack:
+            # 如果节点存在就压栈，然后左走
+            if root:
+                stack.append(root)
+                root = root.left    #最后这个节点会变成None，然后进入else
+            # 如果节点是None
+            else:
+                root = stack.pop()
+                result.append(root.val)
+                root = root.right
+
+        return result
